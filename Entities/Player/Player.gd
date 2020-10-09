7a898201d74130b9c1e3c0a9e27a374dd454cdb5
Michal Biernacki <userofficeit@gmail.com>
2020-10-09 18:19:10 +0200

    Fix mistypes

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -4,7 +4,7 @@ export var health = 100
 export var movement_speed = 300
 
 onready var  animationTree = $AnimationTree
-onready var animtationState = animationTree.get('parameters/playback')
+onready var animationState = animationTree.get('parameters/playback')
 
 func listen_inputs(delta):
 	var velocity = Vector2()
@@ -22,7 +22,7 @@ func listen_inputs(delta):
 	if Input.is_action_just_pressed("Attack"):
 		velocity = Vector2(0, 0)
 		attack()
-	elif(animtationState.get_current_node() != "Attack"):
+	elif(animationState.get_current_node() != "Attack"):
 		position += velocity * delta
 		rotate_to_mouse(velocity)
 
@@ -31,7 +31,7 @@ func attack():
 	var mouse_pos = get_global_mouse_position()
 	var mouse_vector = mouse_pos - self.global_position
 	animationTree.set('parameters/Attack/blend_position', mouse_vector)
-	animtationState.travel("Attack")
+	animationState.travel("Attack")
 	
 func rotate_to_mouse(velocity):
 	var mouse_pos = get_global_mouse_position()
@@ -40,11 +40,11 @@ func rotate_to_mouse(velocity):
 	if(velocity != Vector2(0, 0)):
 		animationTree.set('parameters/Standing/blend_position', mouse_vector)
 		animationTree.set('parameters/Moving/blend_position', mouse_vector)
-		animtationState.travel("Moving")
+		animationState.travel("Moving")
 	else:
 		animationTree.set('parameters/Standing/blend_position', mouse_vector)
 		animationTree.set('parameters/Moving/blend_position', mouse_vector)
-		animtationState.travel("Standing")
+		animationState.travel("Standing")
 
 func _process(delta):
 	listen_inputs(delta)
