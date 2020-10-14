f93854597bb961e97f1ec4dac027914ea770adcb
Ethelt <42093730+Ethelt@users.noreply.github.com>
2020-10-14 19:25:12 +0200

    Added demo of items (#4)
    Items can now be created in code and they modify Player stats

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -3,11 +3,26 @@ extends KinematicBody2D
 export var health = 100
 export var movement_speed = 300
 export var damage_modifier = 0
+export var attack_speed_modifier = 0
 export var magic_bonus = 0
 
-onready var weapon : Node2D
+onready var weapon = $Weapon
 onready var  animationTree = $AnimationTree
 onready var animationState = animationTree.get('parameters/playback')
+
+var Item = preload("res://Items/shared/Item.tscn")
+
+onready var items = {
+	"sword": $Weapon, 
+	"axe": null, 
+	"spear": null, 
+	"chestplate": null, 
+	"helmet": null, 
+	"leggings": null, 
+	"boots": null, 
+	"ring": null,
+	"necklace": null
+}
 
 
 func listen_inputs(delta):
@@ -29,6 +44,13 @@ func listen_inputs(delta):
 	elif(animationState.get_current_node() != "Attack"):
 		move_and_collide(velocity * delta)
 		rotate_to_mouse(velocity)
+	
+	# TODO: Delete this block when testing is over. It's here for development only
+	if Input.is_action_just_pressed("ui_accept"):
+		print("health: ", health)
+		print("movement_speed: ", movement_speed)
+		print("damage_modifier: ", damage_modifier)
+		print("magic_bonus: ", magic_bonus)
 
 
 func attack():
@@ -50,6 +72,21 @@ func rotate_to_mouse(velocity):
 		animationTree.set('parameters/Moving/blend_position', mouse_vector)
 		animationState.travel("Standing")
 
+
+func apply_items_modifiers(item):
+	health += item.hp_modifier
+	movement_speed += item.speed_modifier
+	magic_bonus += item.magic_modifier
+	damage_modifier += item.damage_modifier
+	attack_speed_modifier += item.attack_speed_modifier
+
+
+func _ready():
+	for item in items.keys():
+		item = items[item]
+		if item != null:
+			apply_items_modifiers(item)
+
+
 func _process(delta):
 	listen_inputs(delta)
-	
