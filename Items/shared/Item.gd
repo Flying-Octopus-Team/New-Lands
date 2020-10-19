87192746967d5038c118c60b99455eb99775325f
NoMariusz <60425872+NoMariusz@users.noreply.github.com>
2020-10-19 15:34:41 +0200

    Improvements (#10)
    * Add player demo
    
    Added player and his functionalites like: moving, attacking and following mouse cursor. Also player has his own statistics now.
    
    * Fix mistypes
    
    * Add players camera and change movement function
    
    Added camera following player and changed function for player's movement
    
    * Create enemy demo
    
    Created enemy, his statistics, aggro mechanics and movement function
    
    * Created map generation system
    
    - Created World node, which acts as root node for now
    - Created Map node, which creates the general map and transaltes it into Tilemaps that the player sees
    - World node spawns the player
    - Rooms spawn placeholder enemies inside them
    
    * Integrated map generation with new enemies demo and added comments
    
    * Added short explanation of how the system works
    
    * Map generation (#3)
    
    * Created map generation system
    
    - Created World node, which acts as root node for now
    - Created Map node, which creates the general map and translates it into Tilemaps that the player sees
    - World node spawns the player
    - Rooms spawn enemies inside them
    
    * Deleted unnecessary World.tscn file
    
    * Introduce elements necessary for items and attacking
    
    Controled while writing, so code review wasn't needed.
    
    * Added demo of items (#4)
    
    Items can now be created in code and they modify Player stats
    
    * Added simple Hub using placeholder graphics. (#5)
    
    It can only generate new map and put player on it.
    
    * Create attack system for player (#6)
    
    Now player can kill enemies. Attack system for enemies will be provided in next commits.
    
    * improveed player move
    
    * Improved sword attack animations
    
    Co-authored-by: Michal Biernacki <userofficeit@gmail.com>
    Co-authored-by: Ethelt <42093730+Ethelt@users.noreply.github.com>
    Co-authored-by: Ethelt <bartlomiej.hada@gmail.com>

---
+++ Items/shared/Item.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+export var type : String
+export var item_name : String
+export var hp_modifier : int
+export var speed_modifier : int
+export var magic_modifier : int
+export var damage_modifier : int
+export var attack_speed_modifier : int
