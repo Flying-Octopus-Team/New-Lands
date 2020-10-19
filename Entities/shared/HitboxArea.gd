89a15d20dd79804e1e152215d39f92fba26763c7
Ethelt <bartlomiej.hada@gmail.com>
2020-10-19 20:17:01 +0200

    Revert "Improvements (#10)"
    This reverts commit 87192746967d5038c118c60b99455eb99775325f.
    
    Reverting accidental commit

--- Entities/shared/HitboxArea.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-signal get_damage(damage)
-
-
-func _on_HitboxArea_area_entered(area):
-	if(area.get_name() == "Weapon"):
-		var total_damage = area.damage + area.get_parent().damage_modifier
-		emit_signal("get_damage", total_damage)
--- Hub/Stations/StartRunStation.gd
+++
@@ -1,6 +0,0 @@
-extends "res://Hub/Stations/shared/Station.gd"
-
-# Changes to World node, therefore starting the run
-# Player gets spawned by World, so there's no need to transfer him
-func station_used():
-	get_tree().change_scene("res://World/World.tscn")
