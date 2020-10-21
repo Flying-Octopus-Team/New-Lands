8309491be54849b823e6b0bc9c3c52d84565f0f4
NoMariusz <mariusz2727@vp.pl>
2020-10-21 16:34:07 +0200

    Added more items

--- Scripts/EquipmentManager.gd
+++ Scripts/EquipmentManager.gd
@@ -31,16 +31,25 @@ const RARITY_COLORS = {
 }
 
 var items= {
-	RARITIES.COMMON: [load("res://Items/Equipment/BaseChestplate.tscn").instance(), 
-	load("res://Items/Equipment/BaseHelmet.tscn").instance(),
-	load("res://Items/Equipment/BaseBoots.tscn").instance(),
-	load("res://Items/Equipment/BaseGloves.tscn").instance(),
-	load("res://Items/Equipment/BaseRing.tscn").instance(),
-	load("res://Items/Equipment/BaseNecklace.tscn").instance()],
-	RARITIES.RARE: [load("res://Items/Equipment/CrusaderHelmet.tscn").instance()],
+	RARITIES.COMMON: [load("res://Items/Equipment/Chestplates/BaseChestplate.tscn").instance(), 
+	load("res://Items/Equipment/Helmets/BaseHelmet.tscn").instance(),
+	load("res://Items/Equipment/Boots/BaseBoots.tscn").instance(),
+	load("res://Items/Equipment/Gloves/BaseGloves.tscn").instance(),
+	load("res://Items/Equipment/Rings/BaseRing.tscn").instance(),
+	load("res://Items/Equipment/Necklaces/BaseNecklace.tscn").instance()],
+	RARITIES.UNCOMMON: [load("res://Items/Equipment/Chestplates/UpgradedChestplate.tscn").instance(), 
+	load("res://Items/Equipment/Helmets/UpgradedHelmet.tscn").instance(),
+	load("res://Items/Equipment/Boots/UpgradedBoots.tscn").instance(),
+	load("res://Items/Equipment/Gloves/UpgradedGloves.tscn").instance(),
+	load("res://Items/Equipment/Rings/GoldKnightRing.tscn").instance(),
+	load("res://Items/Equipment/Necklaces/GoldKnightNecklace.tscn").instance()],
+	RARITIES.RARE: [load("res://Items/Equipment/Helmets/CrusaderHelmet.tscn").instance(),
+	load("res://Items/Equipment/Rings/BattlemageRing.tscn").instance()],
+	RARITIES.EPIC: [load("res://Items/Equipment/Necklaces/MerlinsApprenticeNacklace.tscn").instance()],
+	RARITIES.LEGENDARY: [load("res://Items/Equipment/Helmets/HelmetOfAvalonTears.tscn").instance()],
 }
 
-var chest = items[RARITIES.COMMON]
+var chest = [items[RARITIES.COMMON][0]]
 
 var worn_items = {
 	"Sword": load("res://Items/Weapons/Swords/BaseSword.tscn").instance(),
