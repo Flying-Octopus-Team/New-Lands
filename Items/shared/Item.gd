3f9bbeee8a4461983060f592a13db0877da64fd7
Ethelt <42093730+Ethelt@users.noreply.github.com>
2020-10-22 21:13:35 +0200

    Items now drop from enemies (#19)
    Can be picked up by pressing space

--- Items/shared/Item.gd
+++ Items/shared/Item.gd
@@ -9,3 +9,19 @@ export var speed_modifier : int
 export var magic_modifier : int
 export var damage_modifier : int
 export var attack_speed_modifier : float
+var player_nearby = false
+
+func _process(delta):
+	if player_nearby:
+		if Input.is_action_just_pressed("ui_accept"):
+			EquipmentManager.chest.append(duplicate())
+			queue_free()
+
+func _on_Item_body_entered(body):
+	if body.name == "Player":
+		player_nearby = true
+
+
+func _on_Item_body_exited(body):
+	if body.name == "Player":
+		player_nearby = false
--- Scripts/EquipmentManager.gd
+++ Scripts/EquipmentManager.gd
@@ -31,25 +31,25 @@ const RARITY_COLORS = {
 }
 
 var items= {
-	RARITIES.COMMON: [load("res://Items/Equipment/Chestplates/BaseChestplate.tscn").instance(), 
-	load("res://Items/Equipment/Helmets/BaseHelmet.tscn").instance(),
-	load("res://Items/Equipment/Boots/BaseBoots.tscn").instance(),
-	load("res://Items/Equipment/Gloves/BaseGloves.tscn").instance(),
-	load("res://Items/Equipment/Rings/BaseRing.tscn").instance(),
-	load("res://Items/Equipment/Necklaces/BaseNecklace.tscn").instance()],
-	RARITIES.UNCOMMON: [load("res://Items/Equipment/Chestplates/UpgradedChestplate.tscn").instance(), 
-	load("res://Items/Equipment/Helmets/UpgradedHelmet.tscn").instance(),
-	load("res://Items/Equipment/Boots/UpgradedBoots.tscn").instance(),
-	load("res://Items/Equipment/Gloves/UpgradedGloves.tscn").instance(),
-	load("res://Items/Equipment/Rings/GoldKnightRing.tscn").instance(),
-	load("res://Items/Equipment/Necklaces/GoldKnightNecklace.tscn").instance()],
-	RARITIES.RARE: [load("res://Items/Equipment/Helmets/CrusaderHelmet.tscn").instance(),
-	load("res://Items/Equipment/Rings/BattlemageRing.tscn").instance()],
-	RARITIES.EPIC: [load("res://Items/Equipment/Necklaces/MerlinsApprenticeNacklace.tscn").instance()],
-	RARITIES.LEGENDARY: [load("res://Items/Equipment/Helmets/HelmetOfAvalonTears.tscn").instance()],
+	RARITIES.COMMON: [load("res://Items/Equipment/Chestplates/BaseChestplate.tscn"), 
+	load("res://Items/Equipment/Helmets/BaseHelmet.tscn"),
+	load("res://Items/Equipment/Boots/BaseBoots.tscn"),
+	load("res://Items/Equipment/Gloves/BaseGloves.tscn"),
+	load("res://Items/Equipment/Rings/BaseRing.tscn"),
+	load("res://Items/Equipment/Necklaces/BaseNecklace.tscn")],
+	RARITIES.UNCOMMON: [load("res://Items/Equipment/Chestplates/UpgradedChestplate.tscn"), 
+	load("res://Items/Equipment/Helmets/UpgradedHelmet.tscn"),
+	load("res://Items/Equipment/Boots/UpgradedBoots.tscn"),
+	load("res://Items/Equipment/Gloves/UpgradedGloves.tscn"),
+	load("res://Items/Equipment/Rings/GoldKnightRing.tscn"),
+	load("res://Items/Equipment/Necklaces/GoldKnightNecklace.tscn")],
+	RARITIES.RARE: [load("res://Items/Equipment/Helmets/CrusaderHelmet.tscn"),
+	load("res://Items/Equipment/Rings/BattlemageRing.tscn")],
+	RARITIES.EPIC: [load("res://Items/Equipment/Necklaces/MerlinsApprenticeNacklace.tscn")],
+	RARITIES.LEGENDARY: [load("res://Items/Equipment/Helmets/HelmetOfAvalonTears.tscn")],
 }
 
-var chest = [items[RARITIES.COMMON][0]]
+var chest = [items[RARITIES.COMMON][0].instance()]
 
 var worn_items = {
 	"Sword": load("res://Items/Weapons/Swords/BaseSword.tscn").instance(),
